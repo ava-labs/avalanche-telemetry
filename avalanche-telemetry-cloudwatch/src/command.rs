2e9b6fea246d09ffcb838474bc5d511d991e6cfd
Gyuho Lee <gyuho.lee@avalabs.org>
2022-09-19 22:21:14 -0700

    print version
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- avalanche-telemetry-cloudwatch/src/command.rs
+++ avalanche-telemetry-cloudwatch/src/command.rs
@@ -113,6 +113,8 @@ pub struct Flags {
 }
 
 pub async fn execute(opts: Flags) -> io::Result<()> {
+    println!("{} version: {}", NAME, crate_version!());
+
     // ref. https://github.com/env-logger-rs/env_logger/issues/47
     env_logger::init_from_env(
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, opts.log_level),
